a55a2dd2851b7758a75af45ae8878e055341f73d
Thomas Carlson <thomas.oca@gmail.com>
2015-11-16 17:10:58 -0500

    Link values to state values

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -10,6 +10,25 @@ import SenateActions from '../actions/SenateActions';
 class Edit extends BaseComponent {
   constructor() {
     super();
+
+    this.state = {
+      id: 1,
+      bill_id: 'hr1599-114',
+      bill_title: '',
+      bill_desc: '',
+      vote_favor: '',
+      chamber: '',
+      impact_text: '',
+      no_cosponsor_text: '',
+      cosponsor_post_text: '',
+      represent: '',
+      single_voted_for: '',
+      single_voted_against: ''
+    }
+  }
+
+  _handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
   _handleClick() {
@@ -17,22 +36,22 @@ class Edit extends BaseComponent {
     .post('api/settings/edit')
     .send({
       id: 1,
-      bill_id: "hr1599-114",
-      bill_title: "test 123",
-      bill_desc: "test 456",
-      vote_favor: "Yea",
-      chamber: "house",
+      bill_id: this.state.bill_id,
+      bill_title: this.state.bill_title,
+      bill_desc: this.state.bill_desc,
+      vote_favor: this.state.vote_favor,
+      chamber: this.state.chamber,
       created_at: Date.now(),
       senate: {
-        impact_text: 'test 789',
-        no_cosponsor_title: 'test 101112',
-        cosponsor_post_text: 'test 131415',
-        represent: 'test 161718'
+        impact_text: this.state.impact_text,
+        no_cosponsor_title: this.state.no_cosponsor_title,
+        cosponsor_post_text: this.state.cosponsor_post_text,
+        represent: this.state.represent
       },
       house: {
-        single_voted_for: 'test 192021',
-        single_voted_against: 'test 222324',
-        multiple_results: 'test 252627'
+        single_voted_for: this.state.single_voted_for,
+        single_voted_against: this.state.single_voted_against,
+        multiple_results: 'test 252'
       }
     })
     .set('Accept', 'application/json')
