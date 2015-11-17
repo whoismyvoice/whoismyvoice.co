447765f749bc02c2f45d937deb041deafb532344
Thomas Carlson <thomas.oca@gmail.com>
2015-11-17 14:59:41 -0500

    Add further input fields to edit componetn

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,14 +4,16 @@ import request from 'superagent';
 // Components
 import BaseComponent from './BaseComponent';
 import SenateActions from '../actions/SenateActions';
+import Dropdown from 'react-dropdown';
 
 class Edit extends BaseComponent {
   constructor() {
     super();
+    this._bind('_onChangeID', '_onSelectChamber', '_onChangeBillTitle', '_onChangeBillDesc', '_onSelect');
 
     this.state = {
       id: 1,
-      bill_id: 'hr1599-114',
+      bill_id: '',
       bill_title: '',
       bill_desc: '',
       vote_favor: '',
@@ -25,8 +27,28 @@ class Edit extends BaseComponent {
     }
   }
 
-  _handleChange(event) {
-    this.setState({value: event.target.value});
+  _onSelect(option) {
+    this.setState({vote_favor: option.value});
+  }
+
+  _onSelectChamber(option) {
+    this.setState({chamber: option.value});
+  }
+
+  _onChangeID(event) {
+    this.setState({bill_id: event.target.value});
+  }
+
+  _onChangeBillTitle(event) {
+    this.setState({bill_title: event.target.value});
+  }
+
+  _onChangeBillDesc(event) {
+    this.setState({bill_desc: event.target.value});
+  }
+
+  _onChangeVoteFavor(event) {
+    this.setState({vote_favor: event.target.value});
   }
 
   _handleClick() {
@@ -60,7 +82,57 @@ class Edit extends BaseComponent {
   }
 
   render() {
+
+    const VoteOptions = [
+      { value: '1', label: 'Yea'},
+      { value: '0', label: 'Nay'}
+    ];
+
+    const ChamberOptions = [
+      { value: '1', label: 'Senate'},
+      {value: '0', label: 'House'}
+    ];
+
+    let defaultOption = this.state.selected;
+
   	return <div className="page-block">
+    Bill ID
+      <input
+        type="text"
+        placeholder="Enter bill id"
+        onChange={this._onChangeID}
+        value={this.state.bill_id}
+      />
+
+      Bill Title
+      <input
+        type="text"
+        placeholder="Enter bill title"
+        onChange={this._onChangeBillTitle}
+        value={this.state.bill_title}
+      />
+
+      Bill Description
+      <input
+        type="text"
+        placeholder="Enter bill description"
+        onChange={this._onChangeBillDesc}
+        value={this.state.bill_desc}
+      />
+
+      What vote to put in favor?
+      <Dropdown
+        options={VoteOptions}
+        onChange={this._onSelect}
+        placeholder="Select an option"
+      />
+
+      Select chamber
+      <Dropdown
+        options={ChamberOptions}
+        onChange={this._onSelectChamber}
+        placeholder="Select a chamber"
+      />
 
       <br />
       <button onClick={this._handleClick}>Save changes</button>
