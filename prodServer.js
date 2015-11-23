ccc9e86213765863bebd387c65eef8a22db7868b
Thomas Carlson <thomas.oca@gmail.com>
2015-11-23 16:57:29 -0500

    Refactor prodServer to fix bug which caused api/settings not to be reachable

--- prodServer.js
+++ prodServer.js
@@ -9,6 +9,7 @@ import bodyParser from 'body-parser';
 
 mongoose.connect(config.database);
 mongoose.connection.on('open', function() {
+
   // ONLY FOR TESTING PURPOSESE - DROPS DATABASE AND INSTANTIATES NEW SETTINGS
   mongoose.connection.db.dropDatabase();
   const defSet = new Settings(defaultSettings);
@@ -27,17 +28,11 @@ const port = 8080;
 
 server.use(bodyParser.json());
 server.use(helmet());
--server.use(express.static(__dirname + '/dist'));
-
-server.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-});
+server.use(express.static(__dirname + '/dist'))
 
 server.get('/api/settings', function(req, res, next) {
   Settings.findOne({}, {}, {sort: {'created_at': -1 }}, function(err, settings) {
-    if(err) {
-      res.status(400).send('Invalid request');
-    }
+    if(err) return next(err);
     console.info("Received request");
     res.send({settings: settings});
   });
@@ -47,13 +42,15 @@ server.post('/api/settings/edit', function(req, res, next) {
   var post = new Settings(req.body);
   console.info(post);
   post.save(function (err, post) {
-    if(err) {
-      res.status(400).send('Invalid request');
-    }
-    res.status(201).json(post);
-    console.info("Posted new settings successfully");
+    if(err) { return next(err) }
+      res.status(201).json(post);
+      console.info("Posted new settings successfully");
   })
 });
+
+server.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
 server.use(express.static('client/dist'));
 
