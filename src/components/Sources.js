4cd760fe6836612a08d9da4cba36b8753b7597f5
Thomas Carlson <thomas.oca@gmail.com>
2015-12-09 21:45:56 -0500

    Initial House PP setup

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -17,14 +17,9 @@ class Sources extends BaseComponent {
         <p className="page-introduction">
           The data on this website has been lifted directly from the Congress.gov roll call for December 2015, with contact information provided via API's developed by the Sunlight Foundation.<br /><br />
           <br /><br />
-          On December 4, 2015, the Senate voted on H.R 3762, a bill in which Republicans attempted to use the budget reconciliation process to repeal the Affordable Healthcare Act (aka Obamacare) and end federal funding for Planned Parenthood. Budget reconciliation is the one chance each year that the majority (Republican) party gets to bypass the Senate filibuster in order to get a bill to the President's desk, without needing a single vote from the minority (Democrat) party.
-          <br /><br />
-          <span className="resources">
-            Additional resource data was pulled directly from the following sources:
-            <br /><br />
-            1) <a href="https://api.open.fec.gov/" target="_blank">FEC.gov</a><br />
-            2) <a href="http://sunlightfoundation.com/" target="_blank">Sunlight Foundation</a>
-		      </span>
+          On October 23, 2015 the House voted on H.R 3762, a bill in which Republicans attempted to use the budget reconciliation process to repeal the Affordable Healthcare Act (aka Obamacare) and end federal funding for Planned Parenthood. Budget reconciliation is the one chance each year that the majority (Republican) party gets to bypass the Senate filibuster in order to get a bill to the President's desk, without needing a single vote from the minority (Democrat) party.<br /><br />
+          The Republican congressional representatives listed on this website who belong to the House Freedom Caucus (HFC) have publicly declared their willingness to shut down the U.S. government over the issue of Planned Parenthood funding. On September 10, 2015, Congressman Jim Jordan issued the following press release on behalf of the HFC:<br /><br />
+          "Given the appalling revelations surrounding Planned Parenthood, we cannot in good moral conscience vote to send taxpayer money to this organization, while still fulfilling our duty to represent our constituents. We must therefore oppose any spending measure that contains funding for Planned Parenthood."
         </p>
       </div>
     </div>;
