aee451241b495a572860c7742a14be30fc593936
Thomas Carlson <thomas.oca@gmail.com>
2016-01-04 09:40:16 -0700

    Optimize height calculation for page-block and update the sort functions used based on number of members who voted in favor

--- src/utils/CongressUtils.js
+++ src/utils/CongressUtils.js
@@ -13,6 +13,7 @@ const getMemberDetails = (zipCode, lng, senate_votes, house_votes) => {
   .end((err, res) => {
     if (err) return console.error(err);
     let house_members = 0;
+    let membersVoteFavor = 0;
     const members = res.body.results.filter(member => {
       if (member.bioguide_id in senate_votes || member.bioguide_id in house_votes) {
         member.age = (new Date().getFullYear() - member.birthday.substring(0, 4));
@@ -23,17 +24,23 @@ const getMemberDetails = (zipCode, lng, senate_votes, house_votes) => {
         if (senate_votes[member.bioguide_id]) {
           member.voted = senate_votes[member.bioguide_id];
           member.vote_favor = senate_votes[member.bioguide_id] === senate_vote_favor;
+          if (member.vote_favor) {
+            membersVoteFavor++;
+          }
         } else {
           member.voted = house_votes[member.bioguide_id];
           member.vote_favor = house_votes[member.bioguide_id] === house_vote_favor;
           house_members++;
+          if (member.vote_favor) {
+            membersVoteFavor++;
+          }
         }
         return member;
       }
     });
 
     // Sort array of objects based on vote_favor value in order to show members who voted as set vote_favor first
-    const sort_member = function(left, right) {
+    const sort_member_vote = (left, right) => {
       const vote_order = left.vote_favor === right.vote_favor ? 0 : (left.vote_favor < right.vote_favor ? -1 : 1);
       if ((left.vote_favor && right.vote_favor) || (!left.vote_favor && !right.vote_favor)) {
         return vote_order;
@@ -44,10 +51,20 @@ const getMemberDetails = (zipCode, lng, senate_votes, house_votes) => {
       }
     };
 
+    // Sort array based on member's chambers in order to show representatives first
+    const sort_member_chamber = (a, b) => {
+      if (a.chamber < b.chamber) return -1;
+      if (a.chamber > b.chamber) return 1;
+      return 0;
+    };
+
+    // Check whether all members agree based on vote_favor in order to determine sorting func
+    const sort_members = membersVoteFavor > 0 || membersVoteFavor === 3 ? sort_member_vote : sort_member_chamber;
+
     if (res.body.results.length === 0) {
       SenateServerActions.getDetails('error');
     } else if (members.length > 0) {
-      SenateServerActions.getDetails(members.sort(sort_member), members.length, house_members);
+      SenateServerActions.getDetails(members.sort(sort_members), members.length, house_members);
     }
   });
 };
