6ef8bc1482cf3f7b6afe016aae41a58ed6ce4e35
Thomas Carlson <thomas.oca@gmail.com>
2016-01-08 13:41:25 -0500

    Add checks to adjust result text based on payments made by senators/reps

--- src/components/TitleComponent.js
+++ src/components/TitleComponent.js
@@ -45,11 +45,12 @@ class TitleComponent extends BaseComponent {
       voted_for_2,
       voted_against_1,
       voted_against_2,
+      voted_against_3,
+      voted_against_4,
       voted_against,
     } = Settings;
 
     let pre_text,
-      represent_gender,
       preliminary_text,
       member_name = '',
       represent_text,
@@ -57,30 +58,54 @@ class TitleComponent extends BaseComponent {
       action,
       member,
       voted_bold_text,
-      voted_final_text;
+      voted_final_text,
+      voted_link,
+      voted_finish;
 
     if (representative && representative.length === 1) {
-      represent_gender = representative[0].gender_full === 'man' ? 'He' : 'She';
       member_name = representative[0].full_name;
       member = representative[0].chamber === 'senate' ? 'Senator' : 'Representative';
       preliminary_text = representative[0].payment > 0 ? `${voted_for.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action).replace('#bold', '<b>').replace('#bold_end', '</b>')}` : `${voted_against.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action)}`;
       voted_bold_text = representative[0].payment > 0 ? voted_for_1 : voted_against_1;
       voted_final_text = representative[0].payment > 0 ? voted_for_2 : voted_against_2;
+      voted_link = representative[0].payment > 0 ? '' : voted_against_3;
+      voted_finish = representative[0].payment > 0 ? '' : voted_against_4;
     }
 
     if (representative && representative.length === 2) {
-      member = representative[0].chamber === 'senate' ? 'Senators' : 'Representatives';
-      represent_gender = 'These people';
+      const first_member = representative[0];
+      const second_member = representative[1];
+
+      if (first_member.chamber === 'senate' && second_member.chamber === 'senate') {
+        if (first_member.payment > 0 && second_member.payment === 0 || first_member.payment === 0 && second_member.payment > 0) {
+          member = 'Senator';
+        } else {
+          member = 'Senators';
+        }
+      } else if (first_member.chamber === 'house' && second_member.chamber === 'senate') {
+        if (first_member.payment > 0) {
+          member = 'Representative';
+        } else if (second_member.payment > 0) {
+          member = 'Senator';
+        } else {
+          member = 'Representative & Senator';
+        }
+      } else if (first_member.chamber === 'senate' && second_member.chamber === 'house') {
+        if (first_member.payment > 0) {
+          member = 'Senator';
+        } else if (second_member.payment > 0) {
+          member = 'Representative';
+        } else {
+          member = 'Representative & Senator';
+        }
+      }
       member_name = '';
 
-      if (representative[0].chamber === 'house' && representative[1].chamber === 'senate') {
-        member = 'Representative & Senator';
-      } else if (representative[0].chamber === 'senate' && representative[1].chamber === 'house') {
-        member = 'Representative & Senator';
-      }
-      preliminary_text = representative[0].payment > 0 ? `${voted_for.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action)}` : `${voted_against.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action)}`;
-      voted_bold_text = representative[0].payment > 0 ? voted_for_1 : voted_against_1;
-      voted_final_text = representative[0].payment > 0 ? voted_for_2 : voted_against_2;
+      preliminary_text = representative[0].payment > 0 || representative[1].payment > 0 ? `${voted_for.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action)}` : `${voted_against.replace('#member_type', member).replace('#member_name', member_name).replace('#member_age', `a ${representative.age} old`).replace('#member_gender', representative.gender_full).replace('#action', action)}`;
+      voted_bold_text = representative[0].payment > 0 || representative[1].payment > 0 ? voted_for_1 : voted_against_1;
+      voted_final_text = representative[0].payment > 0 || representative[1].payment > 0 ? voted_for_2 : voted_against_2;
+      voted_link = representative[0].payment > 0 || representative[1].payment > 0 ? '' : voted_against_3;
+      voted_finish = representative[0].payment > 0 || representative[1].payment > 0 ? '' : voted_against_4;
     }
 
     // Make sure that vote_question is shown on frontpage and that result text is shown in follow. sections
@@ -123,6 +148,12 @@ class TitleComponent extends BaseComponent {
           <b>{voted_bold_text}</b>
         </span>
         {voted_final_text}
+        <a
+          href="http://www.washingtonpost.com/wp-srv/special/politics/gun-legislation-where-lawmakers-stand/
+        ">
+          {voted_link}
+        </a>
+        {voted_finish}
         <span className={strikeClasses}>
           {vote_question}
         </span>
