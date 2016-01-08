874fec5767b40add9d5ca2da3134fd700846ba9a
Thomas Carlson <thomas.oca@gmail.com>
2016-01-08 11:51:45 -0500

    Add sorting functions in order to show members in correct order based on the contributions made to them, and remove unused values

--- src/utils/SponsorUtils.js
+++ src/utils/SponsorUtils.js
@@ -4,8 +4,15 @@ import SenateConstants from '../constants/SenateConstants';
 import {Settings} from '../constants/SenateConstants';
 
 import SenateStore from '../stores/SenateStore';
+
+// Keep track of members to be able to pass this on to SenateServerActions
 let members = [];
+
+// Keep track of number of house reps. If more than two ask user for street name
 let house_members = 0;
+
+// Keep track of number of reps/sen who got contributions to sort correctly
+let membersContributedTo = 0;
 
 // Promise retrieving amount of money sponsored by NRA to commitee for cycle 2014
 const identifyPayment = (member) => {
@@ -24,6 +31,9 @@ const identifyPayment = (member) => {
         if (member.chamber === 'house') {
           house_members++;
         }
+        if (payment > 0) {
+          membersContributedTo++;
+        }
         const relevant = member;
         resolve(relevant);
       } else {
@@ -33,12 +43,35 @@ const identifyPayment = (member) => {
   });
 };
 
+// Sort array of objects based on payment value in order to show members who got a contribution first
+const sort_member_vote = (left, right) => {
+  const vote_order = left.payment === right.payment ? 0 : (left.payment < right.payment ? -1 : 1);
+  if ((left.payment && right.payment) || (!left.payment && !right.payment)) {
+    return vote_order;
+  } else if (left.payment) {
+    return -1;
+  } else {
+    return 1;
+  }
+};
+
+// Sort array based on member's chambers in order to show representatives first if no contribution received
+const sort_member_chamber = (a, b) => {
+  if (a.chamber < b.chamber) return -1;
+  if (a.chamber > b.chamber) return 1;
+  return 0;
+};
+
+// Check whether all members either received / didn't receive payment in order to use correct sort func
+const sort_members = membersContributedTo > 0 || membersContributedTo === 3 ? sort_member_vote : sort_member_chamber;
+
 // Promise identifying the Committee for each member
 const identifyCommittee = (item) => {
   // Check if did_search is false, and if so truncate it
   if (!SenateStore.getMember.did_search) {
     members.length = 0;
     house_members = 0;
+    membersContributedTo = 0;
   }
 
   const api = `https://api.open.fec.gov/v1/candidate/${item.fec_ids[0]}/committees/?sort_nulls_large=true&api_key=${SenateConstants.FEC_API_KEY}&page=1&per_page=20&designation=P&sort=name&sort_hide_null=true`;
@@ -65,12 +98,11 @@ const identifyCommittee = (item) => {
     members.push(member);
     return Promise.resolve(members);
   }).then(function() {
-    SenateServerActions.getDetails(members, members.length, house_members);
+    SenateServerActions.getDetails(members.sort(sort_members), members.length, house_members);
   });
 };
 
 const memberState = (member) => {
-  member.voted = 'Yea';
   member.full_name = member.middle_name === null ? `${member.first_name} ${member.last_name}` : `${member.first_name} ${member.middle_name} ${member.last_name}`;
   member.gender_full = member.gender === 'M' ? 'man' : 'woman';
   identifyCommittee(member);
