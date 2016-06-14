2fec3e68676861d72392743ce62cd46a7a246e76
Thomas Carlson <thomas@siberia.io>
2016-06-14 11:16:36 -0400

    Update Google API Key

--- src/constants/SenateConstants.js
+++ src/constants/SenateConstants.js
@@ -12,7 +12,7 @@ module.exports = Object.freeze({
   GET_SCHEDULE_A: 'GET_SCHEDULE_A',
   FIND_SPECIFIC_MEMBER: 'FIND_SPECIFIC_MEMBER',
   API_KEY: '4f501d505d514b85a01f39d4ceb9a353',
-  GOOGLE_API_KEY: 'AIzaSyBszQnBTm_2qjgMd57K0CLfE0i3zuveKhE',
+  GOOGLE_API_KEY: 'AIzaSyBnbPmoDZCPvSCv9fJoX12qfr-FO936dZE',
   FEC_API_KEY: process.env.NODE_ENV === 'production' ? 'DNox4ZCBAUvyHH67DnZRxFO0utkiEPMgk1Wpu8mo' : 'innx28Lu1FThi9laPiVZNsXs4Y1egkY5a4eMl9H2',
   Settings: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development' ? SettingsJSON : SenateStore.getSettings()
 });
