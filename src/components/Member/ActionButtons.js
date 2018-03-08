d29ae74cb662e4af1a83834004868a7419bcbc4d
Bryan Swift <bryan@siberia.io>
2018-03-08 15:19:48 -0500

    Remove email action button and retrieve data from `Legislator` getters

--- src/components/Member/ActionButtons.js
+++ src/components/Member/ActionButtons.js
@@ -3,52 +3,38 @@ import PropTypes from 'prop-types';
 
 // Components
 import ContactButtonSmall from './../Buttons/ContactButtonSmall';
+import { Legislator, } from '../../models/Legislator';
 
 // Styles
 import './../../styles/ActionButtons.css';
 
 class SupportActions extends Component {
+  static propTypes = {
+    legislator: PropTypes.instanceOf(Legislator).isRequired,
+  }
+
   render() {
-    let gender,
-      email,
-      twitter,
-      tel,
-      twitterLink;
-
-    const { representative, } = this.props;
-
+    const { legislator, } = this.props;
     // Define each value used for every member
-    if (representative) {
-      gender = representative.gender === 'M' ? 'Him' : 'Her';
-      email = representative.oc_email;
-      twitter = representative.twitter_id;
-      tel = representative.phone;
-      twitterLink = `https://twitter.com/intent/tweet?hashtags=WhoIsMyVoice&text=@${twitter}`;
-    }
-
-    return  <div className="actionButtons">
-      <ContactButtonSmall
-        link={ 'mailto:' + email }
-        icon="email"
-        text={`Email ${gender}`}
-        add_style="smaller"
-      />
-      <ContactButtonSmall
-        link={`tel:${tel}`}
-        icon="phone"
-        text={`Call ${gender}`}
-      />
-      <ContactButtonSmall
-        link={twitterLink}
-        icon="twitter"
-        text={`Tweet at ${gender}`}
-      />
-    </div>;
+    const gender = legislator.genderPronoun;
+    const twitter = legislator.twitter;
+    const tel = legislator.phone;
+    const twitterLink = `https://twitter.com/intent/tweet?hashtags=WhoIsMyVoice&text=@${twitter}`;
+    return (
+      <div className="actionButtons">
+        <ContactButtonSmall
+          link={`tel:${tel}`}
+          icon="phone"
+          text={`Call ${gender}`}
+        />
+        <ContactButtonSmall
+          link={twitterLink}
+          icon="twitter"
+          text={`Tweet at ${gender}`}
+        />
+      </div>
+    );
   }
 }
-
-SupportActions.propTypes = {
-  representative: PropTypes.any,
-};
 
 export default SupportActions;
