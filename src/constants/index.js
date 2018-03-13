243a03f2d121d2379b2515935f6c12138a008dbf
Bryan Swift <bryan@siberia.io>
2018-03-13 17:54:43 -0400

    Redefine the `EXECUTE_PROXY` used

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@
 export const ELECTION_CYCLE = '2016|2018';
 export const ORGANIZATION = 'National Rifle Association';
 export const ORGANIZATION_DISPLAY = 'the NRA';
-export const EXECUTE_PROXY = 'https://gsic7c643d.execute-api.us-east-1.amazonaws.com/production'
+export const EXECUTE_PROXY = 'https://u7zfky1u6d.execute-api.us-east-1.amazonaws.com/production'
