965d8a0848e206ac53c68ea55005dfce7a49e50b
Bryan Swift <bryan@siberia.io>
2018-03-14 19:08:55 -0400

    Update `view` state to include address error messaging
    Refs #24
    
    `store/view` now processes `RECEIVE_OFFICIALS` and
    `RECEIVE_OFFICIALS_ERROR` actions to manage the `addressErrorMessage`
    property.

--- src/store/view.test.js
+++ src/store/view.test.js
@@ -1,5 +1,7 @@
 import {
+  receiveOfficialsError,
   toggleMenu,
+  receiveOfficials,
 } from '../actions';
 import view from './view';
 
@@ -11,11 +13,55 @@ describe('initial state', () => {
   it('has the menu closed in initial state', () => {
     expect(initialState.isMenuOpen).toBe(false);
   });
+  it('has an undefined address error message', () => {
+    expect(initialState.addressErrorMessage).toBe(undefined);
+  });
 
   describe('toggleMenu', () => {
     const state = view(initialState, toggleMenu());
     it('opens the menu', () => {
       expect(state.isMenuOpen).toBe(true);
+    });
+  });
+
+  describe('receiveOfficialsError', () => {
+    let receiveOfficialsErrorAction = undefined;
+    let state = initialState;
+    const error = {
+      response: {
+        json: () => {
+          return Promise.resolve({
+            error: { message: 'foo', },
+          });
+        },
+      },
+    };
+    beforeEach(async () => {
+      receiveOfficialsErrorAction = await receiveOfficialsError(error);
+      state = view(initialState, receiveOfficialsErrorAction)
+    })
+    it('has an error', async () => {
+      expect(state.addressErrorMessage).toBe('foo');
+    });
+
+    describe('receiveOfficials', () => {
+      let newState = undefined;
+      beforeEach(() => {
+        newState = view(state, receiveOfficials([]));
+      });
+      it('no longer has an error', async () => {
+        expect(newState.addressErrorMessage).toBe(undefined);
+      });
+    });
+  });
+
+  describe('receiveOfficials', () => {
+    let state = undefined;
+    beforeEach(() => {
+      state = view(initialState, receiveOfficials([]));
+    });
+    it('does not change address error message', async () => {
+      expect(state.addressErrorMessage).toBe(undefined);
     });
   });
 });
