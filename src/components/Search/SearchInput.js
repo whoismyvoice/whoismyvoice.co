4a4771241f29e680c134a36d48c5b659e40cf84e
Bryan Swift <bryan@siberia.io>
2018-03-14 16:24:29 -0400

    Highlight zip code field on malformed input
    Refs #24

--- src/components/Search/SearchInput.js
+++ src/components/Search/SearchInput.js
@@ -4,15 +4,37 @@ import PropTypes from 'prop-types';
 // Assets
 import '../../styles/SearchInput.css';
 
+const isUsZip = /^(\d{5})(-\d{4})?$/;
+
 class SearchInput extends Component {
+  state = {
+    isError: false,
+  }
+
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    const target = event.target;
+    const value = target.value;
+    this.setState({
+      isError: value !== null && value !== '' && !isUsZip.test(value),
+    });
+  }
+
   render() {
     const { name, pattern, placeholder, } = this.props;
+    const { isError, } = this.state;
     return (
       <input
         type="text"
         name={name}
+        className={isError ? 'error' : ''}
         pattern={pattern}
         placeholder={placeholder}
+        onChange={this.onChange}
       />
     );
   }
