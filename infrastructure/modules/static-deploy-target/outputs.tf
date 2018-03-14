b386982dce632b497e21817966f161cbaf9bcb65
Bryan Swift <bryan@siberia.io>
2018-03-14 11:30:20 -0400

    Add more information to outputs for `static-deploy-target`

--- infrastructure/modules/static-deploy-target/outputs.tf
+++ infrastructure/modules/static-deploy-target/outputs.tf
@@ -1,3 +1,19 @@
+output "cloudfront_id" {
+  description = "Id of the CloudFront distribution fronting the S3 bucket."
+  value = "${aws_cloudfront_distribution.wimv_distribution.id}"
+}
+
+output "fqdn" {
+  description = "Fully Qualified Domain Name where distribution can be accessed."
+  value = "${aws_route53_record.wimv_dns.fqdn}"
+}
+
 output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket storing static assets."
   value = "${aws_s3_bucket.wimv_bucket.arn}"
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket storing static assets."
+  value = "${aws_s3_bucket.wimv_bucket.id}"
 }
--- infrastructure/server/staging/outputs.tf
+++ infrastructure/server/staging/outputs.tf
@@ -1,3 +1,19 @@
-output "s3_wimv_staging_arn" {
+output "cloudfront" {
+  description = "Id of the CloudFront distribution fronting the S3 bucket."
+  value = "${module.deployment.cloudfront_id}"
+}
+
+output "hostname" {
+  description = "Fully Qualified Domain Name where distribution can be accessed."
+  value = "${module.deployment.fqdn}"
+}
+
+output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket storing static assets."
   value = "${module.deployment.s3_bucket_arn}"
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket storing static assets."
+  value = "${module.deployment.s3_bucket_name}"
 }
