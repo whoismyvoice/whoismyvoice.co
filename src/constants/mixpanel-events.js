7a10457a27b0eecc1ab3975a4272d76736968c54
Bryan Swift <bryan@siberia.io>
2018-03-16 19:19:32 -0400

    Track route components mounting

---
+++ src/constants/mixpanel-events.js
@@ -0,0 +1,6 @@
+/** Argugment Arrays for calls to `mixpanel.track` when `About` mounts */
+export const ABOUT_ROUTE = ['NAVIGATE', { route: 'About' }];
+/** Argugment Arrays for calls to `mixpanel.track` when `Home` mounts */
+export const HOME_ROUTE = ['NAVIGATE', { route: 'Home' }];
+/** Argugment Arrays for calls to `mixpanel.track` when `Sources` mounts */
+export const SOURCES_ROUTE = ['NAVIGATE', { route: 'Sources' }];
