264f1dbb227db15d4e205ac67911595568f05e51
Bryan Swift <bryan@siberia.io>
2018-03-16 19:31:58 -0400

    Don't include mixpanel in the public index markup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import mixpanel from 'mixpanel-browser';
+
 import './styles/globals.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Init MixPanel
+mixpanel.init('582e6e69efcbe83615181fab6f29d7b3');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
