3313a9f71edfc91cdff9acbb3e11864a79621e77
Bryan Swift <bryan@siberia.io>
2018-03-23 13:51:52 -0400

    Use a different mixpanel project in dev vs production

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Init MixPanel
-mixpanel.init('582e6e69efcbe83615181fab6f29d7b3');
+if (process.env.NODE_ENV === 'production') {
+  mixpanel.init('582e6e69efcbe83615181fab6f29d7b3');
+} else {
+  mixpanel.init('50ebae8c5fa7e5222b2271df8b73e91d');
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
