19c9cd5fbf3d2a973b682dfd2279841874ce58b2
Bryan Swift <bryan@siberia.io>
2018-03-26 16:05:51 -0400

    Add snapshot tests for `PaymentCounter`

--- src/components/PaymentCounter/PaymentCounter.tsx
+++ src/components/PaymentCounter/PaymentCounter.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 // Constants
-import { ORGANIZATION_DISPLAY } from '../constants';
+import { ORGANIZATION_DISPLAY } from '../../constants';
 // Style
-import './../styles/paymentCounter.css';
+import '../../styles/paymentCounter.css';
 
 interface Props {
   payment: string;
