f26167f5bffe01d4e3c173bd4e8733bb17289250
Bryan Swift <bryan@siberia.io>
2018-03-26 14:38:38 -0400

    Add snapshot tests for `SearchInput`

---
+++ src/components/Search/SearchInput.test.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as renderer from 'react-test-renderer';
+import SearchInput from './SearchInput';
+
+jest.mock('mixpanel-browser');
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<SearchInput name="zipCode" />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly name', () => {
+  const tree = renderer.create(<SearchInput name="zipCode" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly name and pattern', () => {
+  const tree = renderer
+    .create(<SearchInput name="12345" pattern="[a-z]*" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly name and error message', () => {
+  const tree = renderer
+    .create(<SearchInput name="12345" errorMessage="foo errors" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly name and placeholder', () => {
+  const tree = renderer
+    .create(<SearchInput name="12345" placeholder="foo errors" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
