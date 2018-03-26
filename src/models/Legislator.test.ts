42e215a07a50c579eb38e30fd2832d730ee648fd
Bryan Swift <bryan@siberia.io>
2018-03-25 20:47:57 -0400

    Add test helper functions to `Contribution.test` and `Legislator.test`

---
+++ src/models/Legislator.test.ts
@@ -0,0 +1,36 @@
+import { Record, TermRecord, Legislator } from './Legislator';
+
+export function createTerm(): TermRecord {
+  return {
+    type: 'senate',
+    start: '2017-03-03',
+    end: '2017-03-03',
+    state: 'NY',
+    party: 'I',
+    phone: '123-123-1234',
+  };
+}
+
+export function createLegislator(name: string, photoUrl?: string): Record {
+  return {
+    bio: {
+      birthday: '',
+      gender: '',
+    },
+    channels: [],
+    id: {
+      bioguide: '',
+      fec: [],
+    },
+    name: {
+      official_full: name,
+    },
+    photoUrl,
+    terms: [createTerm()],
+  };
+}
+
+it('creates an instance', () => {
+  const instance = new Legislator(createLegislator('John Smith'));
+  expect(instance).toBeInstanceOf(Legislator);
+});
