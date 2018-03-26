6872fb67d638485692aa6784956d7c330b741a70
Bryan Swift <bryan@siberia.io>
2018-03-26 16:27:27 -0400

    Add snapshot tests for `Results` component
    And move the `connect` handling into `Results/index.ts`

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,25 +1,24 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
 import * as cx from 'classnames';
 
 // Components
-import TextButton from './Buttons/TextButton';
-import MemberResults from './MemberResults';
-import { Contribution } from '../models/Contribution';
-import { Legislator, Record as LegislatorRecord } from '../models/Legislator';
-import { reset } from '../actions';
-import { Dispatch } from '../actions/types';
-import { State } from '../store';
+import TextButton from '../Buttons/TextButton';
+import MemberResults from '../MemberResults';
+import { Contribution } from '../../models/Contribution';
+import {
+  Legislator,
+  Record as LegislatorRecord,
+} from '../../models/Legislator';
 
-interface StateProps {
+export interface StateProps {
   didSearch?: boolean;
 }
 
-interface DispatchProps {
+export interface DispatchProps {
   onBack?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
-interface Props extends StateProps, DispatchProps {
+export interface Props extends StateProps, DispatchProps {
   contributions: Array<Contribution>;
   representatives: Array<LegislatorRecord>;
 }
@@ -79,21 +78,3 @@ export class Results extends React.Component<Props> {
     );
   }
 }
-
-function stateToProps(state: State): StateProps {
-  const { address } = state;
-  return {
-    didSearch: address.value !== undefined,
-  };
-}
-
-function dispatchToProps(dispatch: Dispatch): DispatchProps {
-  return {
-    onBack: (event: React.MouseEvent<HTMLAnchorElement>) => {
-      event.preventDefault();
-      dispatch(reset());
-    },
-  };
-}
-
-export default connect(stateToProps, dispatchToProps)(Results);
