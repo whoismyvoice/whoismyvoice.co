d3a898dceaebf40abd4fad092786e3da04147236
Bryan Swift <bryan@siberia.io>
2018-04-02 13:13:04 -0400

    Enable specifying profile with a parameter

--- .circleci/scripts/build-env.sh
+++ .circleci/scripts/build-env.sh
@@ -4,10 +4,11 @@ set -eux
 
 FILTER='.Parameters[] | ("REACT_APP_") + (.Name | sub("/[a-z]+?/"; "") | ascii_upcase) + ("=") + (.Value)'
 ENVIRONMENT=${1:-"production development test"}
+PROFILE=${2:-"whoismyvoice"}
 
 for env in ${ENVIRONMENT}
 do
-aws ssm get-parameters-by-path --profile=whoismyvoice --path=/${env} --recursive \
+aws ssm get-parameters-by-path --profile=${PROFILE} --path=/${env} --recursive \
   | jq --raw-output "${FILTER}" \
   > .env.${env}
 done
