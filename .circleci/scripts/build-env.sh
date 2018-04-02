4eafe2d7020d25bb5b9f537f2a6354d9d57e119a
Bryan Swift <bryan@siberia.io>
2018-04-02 13:31:01 -0400

    If profile is `NONE` then don't use --profile flag

--- .circleci/scripts/build-env.sh
+++ .circleci/scripts/build-env.sh
@@ -6,9 +6,19 @@ FILTER='.Parameters[] | ("REACT_APP_") + (.Name | sub("/[a-z]+?/"; "") | ascii_u
 ENVIRONMENT=${1:-"production development test"}
 PROFILE=${2:-"whoismyvoice"}
 
+# Define where pip executables are located based on `TARGET_ENV`
+PIP_EXEC_PATH="${HOME}/.local/bin"
+
+# If profile is explicitly passed as NONE then don't use profile with `aws`
+if [ "NONE" = $PROFILE ]; then
+  PROFILE_ARG=""
+else
+  PROFILE_ARG="--profile=${PROFILE}"
+fi
+
 for env in ${ENVIRONMENT}
 do
-aws ssm get-parameters-by-path --profile=${PROFILE} --path=/${env} --recursive \
+${PIP_EXEC_PATH}/aws ssm get-parameters-by-path ${PROFILE_ARG} --path=/${env} --recursive \
   | jq --raw-output "${FILTER}" \
   > .env.${env}
 done
