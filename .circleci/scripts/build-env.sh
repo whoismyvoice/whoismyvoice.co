caaf0470ee1394780b8b2b122e79eb17103608c5
Bryan Swift <bryan@siberia.io>
2018-04-07 15:44:41 -0400

    Change how we look for the `aws` executable
    Assume it is in the PATH but if there is a version in `~/.local/bin`
    then use it instead.

--- .circleci/scripts/build-env.sh
+++ .circleci/scripts/build-env.sh
@@ -7,11 +7,15 @@ ENVIRONMENT=${1:-"production development test"}
 PROFILE=${2:-"whoismyvoice"}
 
 # Define where pip executables are located based on `TARGET_ENV`
-PIP_EXEC_PATH="${HOME}/.local/bin"
+PIP_AWS_PATH="${HOME}/.local/bin/aws"
+AWS="aws"
+if [ -x "${PIP_AWS_PATH}" ]; then
+  AWS="${PIP_AWS_PATH}"
+fi
 
 for env in ${ENVIRONMENT}
 do
-${PIP_EXEC_PATH}/aws ssm get-parameters-by-path --profile=${PROFILE} --path=/${env} --recursive \
+$AWS ssm get-parameters-by-path --profile=${PROFILE} --path=/${env} --recursive \
   | jq --raw-output "${FILTER}" \
   > .env.${env}
 done
