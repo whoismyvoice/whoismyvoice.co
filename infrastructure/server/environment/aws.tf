98f2a2e1265fdcc8d6253557d7015a7f29405f34
Bryan Swift <bryan@siberia.io>
2018-04-07 15:02:54 -0400

    Remove Terraform SSM parameter management plan

--- infrastructure/server/environment/aws.tf
+++
@@ -1,7 +0,0 @@
-# Configure the AWS provider
-provider "aws" {
-  version    = "~> 1.8"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region_id}"
-}
