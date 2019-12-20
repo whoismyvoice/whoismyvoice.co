7b1c5bf18b4fc1a7b76976703bcbbc599b2430fc
Bryan Swift <mail@bryanwrit.es>
2019-12-19 21:10:49 -0600

    Upgrade the `global/locktable` plan for terraform 0.12

--- infrastructure/global/locktable/aws.tf
+++ infrastructure/global/locktable/aws.tf
@@ -1,7 +1,7 @@
 # Configure the AWS provider
 provider "aws" {
-  version    = "~> 1.8"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region_id}"
+  version    = "~> 2.43.0"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region_id
 }
--- infrastructure/global/locktable/outputs.tf
+++ infrastructure/global/locktable/outputs.tf
@@ -1,9 +1,9 @@
 output "locktable_wimv_state_arn" {
   description = "The ARN of the standup state lock table."
-  value       = "${aws_dynamodb_table.wimv_locktable.arn}"
+  value       = aws_dynamodb_table.wimv_locktable.arn
 }
 
 output "locktable_wimv_state_name" {
   description = "The name of the standup state lock table."
-  value       = "${aws_dynamodb_table.wimv_locktable.name}"
+  value       = aws_dynamodb_table.wimv_locktable.name
 }
