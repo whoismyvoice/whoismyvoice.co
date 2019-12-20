c4bd95d5d3556468d5c724614758969b27474ffa
Bryan Swift <mail@bryanwrit.es>
2019-12-19 22:17:10 -0600

    Upgrade the `server/production` plan for terraform 0.12

--- infrastructure/server/production/outputs.tf
+++ infrastructure/server/production/outputs.tf
@@ -1,19 +1,19 @@
 output "cloudfront" {
   description = "Id of the CloudFront distribution fronting the S3 bucket."
-  value       = "${module.deployment.cloudfront_id}"
+  value       = module.deployment.cloudfront_id
 }
 
 output "hostname" {
   description = "Fully Qualified Domain Name where distribution can be accessed."
-  value       = "${module.deployment.fqdn}"
+  value       = module.deployment.fqdn
 }
 
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket storing static assets."
-  value       = "${module.deployment.s3_bucket_arn}"
+  value       = module.deployment.s3_bucket_arn
 }
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket storing static assets."
-  value       = "${module.deployment.s3_bucket_name}"
+  value       = module.deployment.s3_bucket_name
 }
