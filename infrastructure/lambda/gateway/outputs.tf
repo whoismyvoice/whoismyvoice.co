854e42186a5bba5a8463d700f953071f724ebd88
Bryan Swift <mail@bryanwrit.es>
2019-12-19 20:56:41 -0600

    Upgrade the `lambda/gateway` plan for terraform 0.12

--- infrastructure/lambda/gateway/outputs.tf
+++ infrastructure/lambda/gateway/outputs.tf
@@ -1,9 +1,13 @@
 output "production_base_url" {
-  value       = "${replace("${aws_api_gateway_deployment.wimv_api_testing_deployment.invoke_url}", "/${aws_api_gateway_deployment.wimv_api_testing_deployment.stage_name}$/", "${aws_api_gateway_stage.wimv_api_production.stage_name}")}"
+  value = replace(
+    aws_api_gateway_deployment.wimv_api_testing_deployment.invoke_url,
+    "/${aws_api_gateway_deployment.wimv_api_testing_deployment.stage_name}$/",
+    aws_api_gateway_stage.wimv_api_production.stage_name,
+  )
   description = "The invokation URL of the production API."
 }
 
 output "testing_base_url" {
-  value       = "${aws_api_gateway_deployment.wimv_api_testing_deployment.invoke_url}"
+  value       = aws_api_gateway_deployment.wimv_api_testing_deployment.invoke_url
   description = "The invokation URL of the testing API."
 }
