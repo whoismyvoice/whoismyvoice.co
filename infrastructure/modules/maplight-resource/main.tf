82557694cdc9a2bd29ed405cbeb746f0523792e0
Bryan Swift <mail@bryanwrit.es>
2019-12-19 20:55:15 -0600

    Upgrade `maplight-resource` module for terraform 0.12

--- infrastructure/modules/maplight-resource/main.tf
+++ infrastructure/modules/maplight-resource/main.tf
@@ -6,18 +6,18 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "maplight_proxy" {
   function_name    = "whoismyvoice_maplight_proxy"
-  filename         = "${data.archive_file.lambda_zip.output_path}"
+  filename         = data.archive_file.lambda_zip.output_path
   handler          = "index.handler"
-  role             = "${var.gateway_lambda_exec_role}"
+  role             = var.gateway_lambda_exec_role
   runtime          = "nodejs6.10"
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
 
 # The API requires at least one "endpoint", or "resource" in AWS terminology.
 # The endpoint created here is: /hello
 resource "aws_api_gateway_resource" "wimv_api_res_maplight" {
-  rest_api_id = "${var.aws_api_gateway_id}"
-  parent_id   = "${var.parent_resource_id}"
+  rest_api_id = var.aws_api_gateway_id
+  parent_id   = var.parent_resource_id
   path_part   = "maplight"
 }
 
@@ -25,21 +25,21 @@ resource "aws_api_gateway_resource" "wimv_api_res_maplight" {
 # a HTTP method (or verb) for that!
 # This is the code for method GET /hello, that will talk to the first lambda
 module "maplight_get" {
-  source                    = "git@github.com:oursiberia/terraform-lambda-gateway.git?ref=v0.0.1//lambda-api-method"
-  api_gateway_rest_api_id   = "${var.aws_api_gateway_id}"
-  api_gateway_resource_id   = "${aws_api_gateway_resource.wimv_api_res_maplight.id}"
+  source                    = "git@github.com:oursiberia/terraform-lambda-gateway.git//lambda-api-method?ref=v0.0.1"
+  api_gateway_rest_api_id   = var.aws_api_gateway_id
+  api_gateway_resource_id   = aws_api_gateway_resource.wimv_api_res_maplight.id
   method                    = "GET"
-  api_gateway_resource_path = "${aws_api_gateway_resource.wimv_api_res_maplight.path}"
-  lambda                    = "${aws_lambda_function.maplight_proxy.function_name}"
-  lambda_invoke_arn         = "${aws_lambda_function.maplight_proxy.invoke_arn}"
-  aws_region_id             = "${var.aws_region_id}"
-  aws_account_id            = "${var.aws_account_id}"
+  api_gateway_resource_path = aws_api_gateway_resource.wimv_api_res_maplight.path
+  lambda                    = aws_lambda_function.maplight_proxy.function_name
+  lambda_invoke_arn         = aws_lambda_function.maplight_proxy.invoke_arn
+  aws_region_id             = var.aws_region_id
+  aws_account_id            = var.aws_account_id
 }
 
 module "maplight_options" {
-  source                    = "git@github.com:oursiberia/terraform-lambda-gateway.git?ref=v0.0.1//cors-api-method"
-  api_gateway_rest_api_id   = "${var.aws_api_gateway_id}"
-  api_gateway_resource_id   = "${aws_api_gateway_resource.wimv_api_res_maplight.id}"
+  source                    = "git@github.com:oursiberia/terraform-lambda-gateway.git//cors-api-method?ref=v0.0.1"
+  api_gateway_rest_api_id   = var.aws_api_gateway_id
+  api_gateway_resource_id   = aws_api_gateway_resource.wimv_api_res_maplight.id
   method                    = "OPTIONS"
-  api_gateway_resource_path = "${aws_api_gateway_resource.wimv_api_res_maplight.path}"
+  api_gateway_resource_path = aws_api_gateway_resource.wimv_api_res_maplight.path
 }
