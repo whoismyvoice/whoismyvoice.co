35ae1cb665ddee3a7be0ffc6a6ca394bb642bc76
Bryan Swift <mail@bryanwrit.es>
2019-12-23 23:17:35 -0600

    Use downloaded election cycle data and fall back to api request

--- src/models/ElectionCycle.ts
+++ src/models/ElectionCycle.ts
@@ -2,3 +2,21 @@ export interface ElectionCycle {
   year: string;
   label: string;
 }
+
+interface ResponseElectionCycle {
+  ElectionCycle: string;
+  label: string;
+}
+
+export function compareElectionCycles(
+  c1: ResponseElectionCycle,
+  c2: ResponseElectionCycle
+) {
+  if (c1.ElectionCycle > c2.ElectionCycle) {
+    return 1;
+  } else if (c1.ElectionCycle < c2.ElectionCycle) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
