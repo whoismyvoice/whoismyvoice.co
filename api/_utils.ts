92cbcd09f241e17491ad9b9dfd68ba9a713562f0
Bryan Swift <mail@bryanwrit.es>
2020-01-23 21:44:52 -0500

    Refactor `#execute` to `#request` to retain and forward headers

--- api/_utils.ts
+++ api/_utils.ts
@@ -1,21 +1,40 @@
 import { get as fetch } from 'https';
+import { IncomingHttpHeaders } from 'http';
+
+/**
+ * The collected result of retrieving HTTP data.
+ */
+export interface Response<T = string> {
+  body: T;
+  headers: IncomingHttpHeaders;
+  method?: string;
+  statusCode?: number;
+  url?: string;
+}
 
 /**
  * Wrap `http.get` in a Promise, collects data chunks as a string and resolves
  * the Promise when the `IncomingMessage` indicates it is 'end'ed.
  * @param url to GET.
- * @returns the string data retrieved.
+ * @returns the response data retrieved.
  * @throws any errors emitted by the http request.
  * @throws Error when upstream request is not a 200 response.
  * @throws Error when upstream request results in a content type that does not
  * match `application/json`.
  */
-export function execute(
+export function request(
   url: string,
-  expects = 'application/json'
-): Promise<string> {
+  expects: string
+): Promise<Response<string>> {
   return new Promise((resolve, reject) => {
     const req = fetch(url, res => {
+      const result = {
+        body: '',
+        headers: res.headers,
+        method: res.method,
+        statusCode: res.statusCode,
+        url: res.url,
+      };
       const statusCode = res.statusCode;
       const contentType = res.headers['content-type'];
       const contentTypeMatcher = new RegExp(expects);
@@ -41,8 +60,9 @@ export function execute(
         let rawData = '';
         res.on('data', chunk => (rawData += chunk));
         res.on('end', () => {
+          result.body = rawData;
           try {
-            resolve(rawData);
+            resolve(result);
           } catch (e) {
             reject(e);
           }
@@ -51,4 +71,21 @@ export function execute(
     });
     req.on('error', e => reject(e));
   });
+}
+
+/**
+ * Wrap `http.get` in a Promise, collects data chunks as a string and resolves
+ * the Promise when the `IncomingMessage` indicates it is 'end'ed.
+ * @param url to GET.
+ * @returns the string data retrieved.
+ * @throws any errors emitted by the http request.
+ * @throws Error when upstream request is not a 200 response.
+ * @throws Error when upstream request results in a content type that does not
+ * match `application/json`.
+ */
+export async function execute(
+  url: string,
+  expects = 'application/json'
+): Promise<string> {
+  return request(url, expects).then(res => res.body);
 }
