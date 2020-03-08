7e1c4255bcc080831ffe6cb38dffccd1863653d4
Bryan Swift <mail@bryanwrit.es>
2020-03-08 18:36:42 -0400

    Create type alias for arguments to `mixpanel.track`

--- src/constants/mixpanel-events.ts
+++ src/constants/mixpanel-events.ts
@@ -1,6 +1,9 @@
+import { track } from 'mixpanel-browser';
+
+type TrackArguments = Parameters<typeof track>;
 /** Argugment Arrays for calls to `mixpanel.track` when `About` mounts */
-export const ABOUT_ROUTE = ['NAVIGATE', { route: 'About' }];
+export const ABOUT_ROUTE: TrackArguments = ['NAVIGATE', { route: 'About' }];
 /** Argugment Arrays for calls to `mixpanel.track` when `Home` mounts */
-export const HOME_ROUTE = ['NAVIGATE', { route: 'Home' }];
+export const HOME_ROUTE: TrackArguments = ['NAVIGATE', { route: 'Home' }];
 /** Argugment Arrays for calls to `mixpanel.track` when `Sources` mounts */
-export const SOURCES_ROUTE = ['NAVIGATE', { route: 'Sources' }];
+export const SOURCES_ROUTE: TrackArguments = ['NAVIGATE', { route: 'Sources' }];
