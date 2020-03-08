f5f5859524c3d448a6e7e17a9ba3223f7b8b581d
Bryan Swift <mail@bryanwrit.es>
2020-03-08 12:20:58 -0400

    Change how error content is retrieved and parsed
    Use optional chaining when constructing a `GoogleResponseError` and use
    type guards when getting the type of error.

--- src/models/GoogleResponseError.ts
+++ src/models/GoogleResponseError.ts
@@ -17,10 +17,6 @@ interface ErrorRecord {
   reason: string | undefined;
 }
 
-function objectHasProp(propName: string, type: string, o: object): boolean {
-  return typeof o === 'object' && typeof o[propName] === type;
-}
-
 /**
  * Extends `Error` accepting a Fetch response in addition to normal `Error`
  * arguments.
@@ -37,42 +33,36 @@ export class GoogleResponseError extends ResponseError {
     Object.setPrototypeOf(this, GoogleResponseError.prototype);
     this.response = response;
     const content: Promise<BodyContent> = this.response.json();
-    this.code = content.then(body => {
-      const error = (body.error === undefined && {}) || body.error!;
-      return (typeof error.code === 'number' && error.code) || 500;
-    });
-    this.errors = content.then(body => {
-      const error = (body.error === undefined && {}) || body.error!;
-      return (error.errors instanceof Array && error.errors) || [];
-    });
-    this.responseMessage = content.then(body => {
-      const error = (body.error === undefined && {}) || body.error!;
-      return (typeof error.message === 'string' && error.message) || 'Error';
-    });
+    this.code = content.then(body => body.error?.code || 500);
+    this.errors = content.then(body => body.error?.errors || []);
+    this.responseMessage = content.then(body => body.error?.message || 'Error');
   }
 
   get isGlobal(): Promise<boolean> {
     return this.errors.then(errors => {
-      const filterFn = objectHasProp.bind(this, 'domain', 'string');
-      return errors
-        .filter(filterFn)
-        .reduce<boolean>((r, err) => r && err.domain! === 'global', true);
+      return errors.reduce<boolean>(
+        (r, err) =>
+          r && typeof err.domain === 'string' && err.domain === 'global',
+        true
+      );
     });
   }
 
   get isParseError(): Promise<boolean> {
     return this.errors.then(errors => {
-      const filterFn = objectHasProp.bind(this, 'reason', 'string');
-      return errors
-        .filter(filterFn)
-        .reduce<boolean>((r, err) => r && err.reason! === 'parseError', true);
+      return errors.reduce<boolean>(
+        (r, err) =>
+          r && typeof err.reason === 'string' && err.reason === 'parseError',
+        true
+      );
     });
   }
 
   get errorMessages(): Promise<Array<string>> {
     return this.errors.then(errors => {
-      const filterFn = objectHasProp.bind(this, 'message', 'string');
-      return errors.filter(filterFn).map(err => err.message!);
+      return errors
+        .map(err => err.message)
+        .filter((m): m is string => typeof m === 'string');
     });
   }
 }
