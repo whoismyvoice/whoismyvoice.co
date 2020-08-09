044806c7bd5928982f6fa060c64495163c12c7c9
Bryan Swift <bryan@bryanjswift.com>
2020-08-09 12:45:57 -0400

    Change `contributions` to use immer

--- src/models/Contribution.ts
+++ src/models/Contribution.ts
@@ -4,24 +4,24 @@ import { BioguideId } from './Legislator';
  * Defines the type alias for a `Contribution` record.
  */
 export interface Contribution {
-  amount: number;
-  legislatorId: BioguideId;
-  organization: string;
+  readonly amount: number;
+  readonly legislatorId: BioguideId;
+  readonly organization: string;
 }
 
 /**
  * Defines a record representing a contribution from a specified sector.
  */
 export interface SectorContribution {
-  amount: number;
-  sector: string;
-  sectorCode: string;
+  readonly amount: number;
+  readonly sector: string;
+  readonly sectorCode: string;
 }
 
 /**
  * Associates a legislatorId with a list of `SectorContribution` records.
  */
 export interface SectorContributions {
-  legislatorId: string;
-  contributions: SectorContribution[];
+  readonly legislatorId: string;
+  readonly contributions: Readonly<SectorContribution[]>;
 }
