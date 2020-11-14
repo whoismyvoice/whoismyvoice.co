6e727b3263e449d8fc635076d9d18972c59956d3
Bryan Swift <bryan@bryanjswift.com>
2020-11-14 14:33:41 -0500

    Convert `TextButton` and `TextFormButton` to function components
    Change `TextFormButton` tests to use `@testing-library/react`

--- src/components/Buttons/TextFormButton.test.tsx
+++ src/components/Buttons/TextFormButton.test.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
 import TextFormButton from './TextFormButton';
 
 const props = {
@@ -8,12 +8,22 @@ const props = {
 };
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TextFormButton {...props} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { container } = render(<TextFormButton {...props} />);
+  expect(container).not.toBeNull();
 });
 
 it('renders correctly', () => {
-  const tree = renderer.create(<TextFormButton {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container, getByText } = render(<TextFormButton {...props} />);
+  expect(container.querySelector('.text-button-border')).toBeInTheDocument();
+  expect(getByText('text')).toBeInTheDocument();
+  expect(getByText('text')).toHaveAttribute('type', 'submit');
+  expect(getByText('text').tagName).toMatch(/button/i);
+});
+
+it('executes a callback when clicked', () => {
+  const onClick = jest.fn();
+  const { getByText } = render(<TextFormButton {...props} onClick={onClick} />);
+  const link = getByText(props.text);
+  fireEvent.click(link);
+  expect(onClick).toHaveBeenCalledTimes(1);
 });
--- src/components/Buttons/TextFormButton.tsx
+++ src/components/Buttons/TextFormButton.tsx
@@ -1,21 +1,19 @@
-import * as React from 'react';
-import TextButton from './TextButton';
+import React, { FC, useMemo } from 'react';
+import cx from 'classnames';
+import { Props } from './TextButton';
 
-class TextFormButton extends TextButton {
-  render(): JSX.Element {
-    const { text } = this.props;
-    const { buttonClasses } = this.state;
-    return (
-      <button
-        type="submit"
-        className={buttonClasses}
-        onClick={this.props.onClick}
-      >
-        {text}
-        <span className="text-button-border" />
-      </button>
-    );
-  }
-}
+export const TextFormButton: FC<Omit<Props, 'link'>> = (props) => {
+  const { onClick = () => void 0, text } = props;
+  const buttonClasses = useMemo(
+    () => cx('text-button', { 'text-button--back': text === 'Back' }),
+    [text]
+  );
+  return (
+    <button type="submit" className={buttonClasses} onClick={onClick}>
+      {text}
+      <span className="text-button-border" />
+    </button>
+  );
+};
 
 export default TextFormButton;
