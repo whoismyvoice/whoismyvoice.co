0f2e782189bd812468c34154c92b8ce0b1a27e49
Bryan Swift <bryan@bryanjswift.com>
2020-11-15 10:31:41 -0500

    Convert `MemberImg` to a function component
    Change the tests for `MemberImg` to use `@testing-library/react`

--- src/components/Member/MemberImg.test.tsx
+++ src/components/Member/MemberImg.test.tsx
@@ -1,22 +1,48 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
 import MemberImg from './MemberImg';
-import { createLegislator } from '../../models/Legislator.test';
-import { Legislator } from '../../models/Legislator';
-
-const props = {
-  legislator: new Legislator(createLegislator('John Smith')),
-  repNumber: 1,
-};
+import { createLegislator, createTerm } from '../../models/Legislator.test';
+import { Legislator, Party } from '../../models/Legislator';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemberImg {...props} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const legislator = new Legislator(createLegislator('John Smith'));
+  const { container, getByAltText } = render(
+    <MemberImg legislator={legislator} />
+  );
+  expect(container).toBeInTheDocument();
+  expect(getByAltText(legislator.fullName)).toBeInTheDocument();
 });
 
-it('renders correctly', () => {
-  const tree = renderer.create(<MemberImg {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+it('renders correctly for an independent', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Independent }],
+  });
+  const { container } = render(<MemberImg legislator={legislator} />);
+  const img = container.querySelector('.member-img');
+  expect(img).toBeInTheDocument();
+  expect(img?.classList.contains('member--grey')).toBe(true);
+});
+
+it('renders correctly for a republican', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Republican }],
+  });
+  const { container } = render(<MemberImg legislator={legislator} />);
+  const img = container.querySelector('.member-img');
+  expect(img).toBeInTheDocument();
+  expect(img?.classList.length).toBe(1);
+});
+
+it('renders correctly for a democrat', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Democrat }],
+  });
+  const { container } = render(<MemberImg legislator={legislator} />);
+  const img = container.querySelector('.member-img');
+  expect(img).toBeInTheDocument();
+  expect(img?.classList.contains('member--blue')).toBe(true);
 });
--- src/components/Member/MemberImg.tsx
+++ src/components/Member/MemberImg.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { VFC } from 'react';
 import cx from 'classnames';
 
 import { Legislator, Party } from '../../models/Legislator';
@@ -8,31 +8,25 @@ import './../../styles/SenatorImg.scss';
 
 interface Props {
   legislator: Legislator;
-  repNumber: number;
+  repNumber?: number;
 }
 
-class SenatorImg extends React.Component<Props> {
-  static defaultProps = {
-    repNumber: 0,
-  };
+export const MemberImg: VFC<Props> = (props) => {
+  const { legislator, repNumber = 0 } = props;
+  const { party } = legislator;
 
-  render(): JSX.Element {
-    const { legislator, repNumber } = this.props;
-    const { party } = legislator;
+  const imgClasses = cx(
+    ['member-img'],
+    { animated: repNumber > 2 },
+    { 'member--blue': party === Party.Democrat },
+    { 'member--grey': party === Party.Independent }
+  );
 
-    const imgClasses = cx(
-      ['member-img'],
-      { animated: repNumber > 2 },
-      { 'member--blue': party === Party.Democrat },
-      { 'member--grey': party === Party.Independent }
-    );
+  return (
+    <div className={imgClasses}>
+      <img alt={legislator.fullName} src={legislator.photoUrl} />
+    </div>
+  );
+};
 
-    return (
-      <div className={imgClasses}>
-        <img alt={legislator.fullName} src={legislator.photoUrl} />
-      </div>
-    );
-  }
-}
-
-export default SenatorImg;
+export default MemberImg;
