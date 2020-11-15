d021e5d433f2d523bac8bb4e04538e8f0dbda82a
Bryan Swift <bryan@bryanjswift.com>
2020-11-15 00:52:51 -0500

    Change `MemberResults` tests to use `@testing-library/react`

--- src/components/MemberResults/MemberResults.test.tsx
+++ src/components/MemberResults/MemberResults.test.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
 import MemberResults from './MemberResults';
 import { Legislator } from '../../models/Legislator';
 import { createLegislator as createLegislatorRecord } from '../../models/Legislator.test';
@@ -11,46 +12,66 @@ function createLegislator(name: string): Legislator {
 it('renders correctly with empty props', () => {
   const props = {
     legislators: [],
-    section: 1,
     sectors: [],
+    sectorContributions: {},
   };
-  const tree = renderer.create(<MemberResults {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container } = render(<MemberResults {...props} />);
+  expect(container.childElementCount).toBe(0);
+  expect(container.querySelector('.title-component--results')).toBeNull();
+  expect(container.querySelector('.member-wrapper')).toBeNull();
 });
 
 it('renders correctly with contribution and legislator', () => {
   const props = {
     legislators: [createLegislator('John Smith')],
-    section: 1,
     sectors: [],
+    sectorContributions: {},
   };
-  const tree = renderer.create(<MemberResults {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container, getByText } = render(<MemberResults {...props} />);
+  expect(
+    container.querySelector('.title-component--results')
+  ).toBeInTheDocument();
+  expect(container.querySelector('.member-wrapper')).toBeInTheDocument();
+  expect(container.querySelectorAll('.member-container').length).toBe(1);
+  expect(getByText('John Smith', { exact: false })).toBeInTheDocument();
 });
 
 it('renders correctly with two contributions and legislators', () => {
   const props = {
     legislators: [
-      createLegislator('John Smith'),
+      createLegislator('John Smith I'),
       createLegislator('John Smith Jr.'),
     ],
-    section: 1,
     sectors: [],
+    sectorContributions: {},
   };
-  const tree = renderer.create(<MemberResults {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container, getByText } = render(<MemberResults {...props} />);
+  expect(
+    container.querySelector('.title-component--results')
+  ).toBeInTheDocument();
+  expect(container.querySelector('.member-wrapper')).toBeInTheDocument();
+  expect(container.querySelectorAll('.member-container').length).toBe(2);
+  expect(getByText('John Smith I', { exact: false })).toBeInTheDocument();
+  expect(getByText('John Smith Jr.', { exact: false })).toBeInTheDocument();
 });
 
 it('renders correctly with three contributions and legislators', () => {
   const props = {
     legislators: [
-      createLegislator('John Smith'),
+      createLegislator('John Smith 1'),
       createLegislator('John Smith Jr.'),
       createLegislator('John Smith III'),
     ],
     section: 1,
     sectors: [],
   };
-  const tree = renderer.create(<MemberResults {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container, getByText } = render(<MemberResults {...props} />);
+  expect(
+    container.querySelector('.title-component--results')
+  ).toBeInTheDocument();
+  expect(container.querySelector('.member-wrapper')).toBeInTheDocument();
+  expect(container.querySelectorAll('.member-container').length).toBe(3);
+  expect(getByText('John Smith 1', { exact: false })).toBeInTheDocument();
+  expect(getByText('John Smith Jr.', { exact: false })).toBeInTheDocument();
+  expect(getByText('John Smith III', { exact: false })).toBeInTheDocument();
 });
