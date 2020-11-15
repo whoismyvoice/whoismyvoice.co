70bbf828a84dff1e3e7ba5a3146fc107c40111e4
Bryan Swift <bryan@bryanjswift.com>
2020-11-15 00:59:10 -0500

    Change `MemberRibbon` to be a function component
    Change tests for `MemberRibbon` to use `@testing-library/react`.

--- src/components/Member/MemberRibbon.test.tsx
+++ src/components/Member/MemberRibbon.test.tsx
@@ -1,21 +1,49 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
 import MemberRibbon from './MemberRibbon';
-import { createLegislator } from '../../models/Legislator.test';
-import { Legislator } from '../../models/Legislator';
+import { createLegislator, createTerm } from '../../models/Legislator.test';
+import { Legislator, Party } from '../../models/Legislator';
 
 const props = {
   legislator: new Legislator(createLegislator('John Smith')),
 };
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemberRibbon {...props} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { container, getByText } = render(<MemberRibbon {...props} />);
+  expect(container.childElementCount).toBeGreaterThan(0);
+  expect(getByText('John Smith', { exact: false })).toBeInTheDocument();
 });
 
-it('renders correctly', () => {
-  const tree = renderer.create(<MemberRibbon {...props} />).toJSON();
-  expect(tree).toMatchSnapshot();
+it('renders correctly for an independent', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Independent }],
+  });
+  const { container } = render(<MemberRibbon legislator={legislator} />);
+  const ribbon = container.querySelector('.ribbon');
+  expect(ribbon).toBeInTheDocument();
+  expect(ribbon?.classList.contains('ribbon--grey')).toBe(true);
+});
+
+it('renders correctly for a republican', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Republican }],
+  });
+  const { container } = render(<MemberRibbon legislator={legislator} />);
+  const ribbon = container.querySelector('.ribbon');
+  expect(ribbon).toBeInTheDocument();
+  expect(ribbon?.classList.length).toBe(1);
+});
+
+it('renders for a democrat', () => {
+  const legislator = new Legislator({
+    ...createLegislator('John Smith'),
+    terms: [{ ...createTerm(), party: Party.Democrat }],
+  });
+  const { container } = render(<MemberRibbon legislator={legislator} />);
+  const ribbon = container.querySelector('.ribbon');
+  expect(ribbon).toBeInTheDocument();
+  expect(ribbon?.classList.contains('ribbon--blue')).toBe(true);
 });
