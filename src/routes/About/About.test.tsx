47752e52bf3d51bf1e21da712e48abb097d653a2
Bryan Swift <bryan@bryanjswift.com>
2020-11-15 11:38:05 -0500

    Change `About` to be a void function component
    Update the tests for `About` to use react testing library.

--- src/routes/About/About.test.tsx
+++ src/routes/About/About.test.tsx
@@ -1,15 +1,23 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import { render } from '@testing-library/react';
 import About from './About';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<About />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders correctly', () => {
-  const tree = renderer.create(<About />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('About', () => {
+  it('renders correctly', () => {
+    const { container, getByText, getByAltText } = render(<About />);
+    expect(container.childElementCount).toBeGreaterThan(0);
+    expect(getByText('About')).not.toBeNull();
+    expect(getByText('questions [at] whoismyvoice [dot] co').tagName).toMatch(
+      /a/i
+    );
+    expect(getByText('contact [at] whoismyvoice [dot] co').tagName).toMatch(
+      /a/i
+    );
+    expect(getByAltText('Siberia').tagName).toMatch(/img/i);
+    expect(
+      getByText('unaffiliated with any government or lobbying party', {
+        exact: false,
+      })
+    ).not.toBeNull();
+  });
 });
--- src/routes/About/About.tsx
+++ src/routes/About/About.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { VFC } from 'react';
 
 // Components
 import FadedBG from '../../components/FadedBg';
@@ -7,56 +7,50 @@ import FadedBG from '../../components/FadedBg';
 import '../../styles/About.scss';
 import siberiaLogo from '../../img/siberia_logo.png';
 
-class About extends React.Component {
-  render(): JSX.Element {
-    return (
-      <div className="page-block light-white">
-        <FadedBG color="white" />
-        <div className="page-wrapper">
-          <h2>About</h2>
-          <p className="page-introduction">
-            This website is unaffiliated with any government or lobbying party
-            or entity, nor is it represented by a media outlet. Its objective is
-            to provide easy access to comprehensive, actionable data to
-            constituents who have a desire to better understand & reach the
-            decision makers who represent them.
-            <br />
-            <br />
-            This website was created as a proactive, in-house exercise by the
-            team at Siberia.
-            <br />
-            <br />
-            Please send any comments or fact checking{' '}
-            <a
-              href="mailto:q%75est&#105;%6f&#110;&#115;@w%68o%69s%6dyvoice%2e%63o?subject=whoismyvoivce.co%20Question"
-              className="strike-out"
-            >
-              questions [at] whoismyvoice [dot] co
-            </a>
-            .
-            <br />
-            <br />
-            Please send any business or media inquiries{' '}
-            <a
-              href="mailto:%63onta%63%74%40w%68%6fi%73%6d%79%76&#111;i%63%65&#46;%63&#111;?subject=whoismyvoivce.co%20Inquiry"
-              className="strike-out"
-            >
-              contact [at] whoismyvoice [dot] co
-            </a>
-            .<br />
-            <br />
-            <a
-              href="http://siberia.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img alt="Siberia" src={siberiaLogo} width="40" />
-            </a>
-          </p>
-        </div>
+export const About: VFC = () => {
+  return (
+    <div className="page-block light-white">
+      <FadedBG color="white" />
+      <div className="page-wrapper">
+        <h2>About</h2>
+        <p className="page-introduction">
+          This website is unaffiliated with any government or lobbying party or
+          entity, nor is it represented by a media outlet. Its objective is to
+          provide easy access to comprehensive, actionable data to constituents
+          who have a desire to better understand & reach the decision makers who
+          represent them.
+          <br />
+          <br />
+          This website was created as a proactive, in-house exercise by the team
+          at Siberia.
+          <br />
+          <br />
+          Please send any comments or fact checking{' '}
+          <a
+            href="mailto:q%75est&#105;%6f&#110;&#115;@w%68o%69s%6dyvoice%2e%63o?subject=whoismyvoivce.co%20Question"
+            className="strike-out"
+          >
+            questions [at] whoismyvoice [dot] co
+          </a>
+          .
+          <br />
+          <br />
+          Please send any business or media inquiries{' '}
+          <a
+            href="mailto:%63onta%63%74%40w%68%6fi%73%6d%79%76&#111;i%63%65&#46;%63&#111;?subject=whoismyvoivce.co%20Inquiry"
+            className="strike-out"
+          >
+            contact [at] whoismyvoice [dot] co
+          </a>
+          .<br />
+          <br />
+          <a href="http://siberia.io" target="_blank" rel="noopener noreferrer">
+            <img alt="Siberia" src={siberiaLogo} width="40" />
+          </a>
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default About;
