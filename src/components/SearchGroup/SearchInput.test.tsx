36bb5b726b5a880e68684c3efd8324ad340ea651
Bryan Swift <bryan@bryanjswift.com>
2020-11-15 22:10:27 -0500

    Change `SearchInput` to a void function component
    Update the `SearchInput` tests to use react testing library

--- src/components/SearchGroup/SearchInput.test.tsx
+++ src/components/SearchGroup/SearchInput.test.tsx
@@ -1,36 +1,51 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as renderer from 'react-test-renderer';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
 import { SearchInput } from './SearchInput';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SearchInput name="zipCode" />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('SearchInput', () => {
+  it('renders with defaults', async () => {
+    const { container } = render(<SearchInput name="zipCode" />);
+    const input = container.querySelector('input');
+    expect(input).not.toBeNull();
+    expect(input).toHaveAttribute('name', 'zipCode');
+    expect(container.querySelector('.search-input-message--error')).toBeNull();
+    // TODO Test that a change event with bad data sets error state
+  });
 
-it('renders correctly name', () => {
-  const tree = renderer.create(<SearchInput name="zipCode" />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it('renders correctly name', () => {
+    const { container } = render(<SearchInput name="zipCode" />);
+    const input = container.querySelector('input');
+    expect(input).not.toBeNull();
+    expect(input).toHaveAttribute('name', 'zipCode');
+    expect(container.querySelector('.search-input-message--error')).toBeNull();
+  });
 
-it('renders correctly name and pattern', () => {
-  const tree = renderer
-    .create(<SearchInput name="12345" pattern="[a-z]*" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it('renders correctly name and pattern', () => {
+    const { container } = render(<SearchInput name="12345" pattern="[a-z]*" />);
+    const input = container.querySelector('input');
+    expect(input).not.toBeNull();
+    expect(input).toHaveAttribute('name', '12345');
+    expect(input).toHaveAttribute('pattern', '[a-z]*');
+    expect(container.querySelector('.search-input-message--error')).toBeNull();
+  });
 
-it('renders correctly name and error message', () => {
-  const tree = renderer
-    .create(<SearchInput name="12345" errorMessage="foo errors" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it('renders correctly name and error message', () => {
+    const { container, getByText } = render(
+      <SearchInput name="12345" errorMessage="foo errors" />
+    );
+    const input = container.querySelector('input');
+    expect(input).not.toBeNull();
+    expect(input).toHaveAttribute('name', '12345');
+    expect(getByText('foo errors')).toHaveClass('search-input-message--error');
+  });
 
-it('renders correctly name and placeholder', () => {
-  const tree = renderer
-    .create(<SearchInput name="12345" placeholder="foo errors" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  it('renders correctly name and placeholder', () => {
+    const { getByPlaceholderText } = render(
+      <SearchInput name="12345" placeholder="foo errors" />
+    );
+    const input = getByPlaceholderText('foo errors');
+    expect(input).not.toBeNull();
+    expect(input).toHaveAttribute('name', '12345');
+  });
 });
